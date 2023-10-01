4da94c31a8fa8ea7b255dba95d546c39fc62e96c
Jeff Jones <jeffbjones777@gmail.com>
2023-10-01 01:29:28 +0100

    Attempt 1

--- primes.py
+++ primes.py
@@ -2,5 +2,22 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if number_of_primes <=0:
+        raise ValueError(f'{number_of_primes} should should be a positive number')
+    else:
+        list = []
+        incremental = 2
+        while len(list) < number_of_primes:
+            is_prime = True
+            for i in range(2, incremental):
+                if incremental%i == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                list.append(incremental)
+            incremental += 1    
+        
     return list
+
+
+
